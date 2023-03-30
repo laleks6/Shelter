65ca9d1f508916cc47eac43ce6f31d9acbc0905c
Aleksandr <96025122+laleks6@users.noreply.github.com>
2023-03-30 18:36:25 +0300

    feat: add adaptive for screen >1280px up to 738px

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log(100+ " баллов\nЕсли вдруг есть недочеты, то пишите в дискорд (@laleks#0503), либо в проверочном чате RS")
+const burgerIcon = document.querySelector(".burger__icon");
+
+burgerIcon.addEventListener('click', () => {
+    burgerIcon.classList.toggle("burger__acrive")
+})
